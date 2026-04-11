071c92cec6f6b402e7c18651dacf178dd6061923
Harsh Kumar <167132722+013harsh@users.noreply.github.com>
2026-04-11 21:19:17 +0530

    f/f/de

--- frontend/frontend/src/component/Details/Experience.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Exprience = () => {
-  return <div>Exprience</div>;
-};
-
-export default Exprience;
--- frontend/frontend/src/component/Details/MyCV.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const MyCV = () => {
-  return <div>MyCV</div>;
-};
-
-export default MyCV;
